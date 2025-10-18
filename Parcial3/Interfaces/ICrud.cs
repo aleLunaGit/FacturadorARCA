a3a1369f8c8eb847dacfbe2c22dffc6a26b6d944
jordansep <jordanmonier4@gmail.com>
2025-10-18 16:51:29 -0300

    #7: CrearInterfaces

---
+++ Parcial3/Interfaces/ICrud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial3.Interfaces
+{
+    public interface ICrud:ICrudFactura
+    {
+        void Down();
+        void Update();
+        void List();
+    }
+    public interface ICrudFactura {
+        void Up();
+        void Read();
+
+    }
+}
---
+++ Parcial3/Interfaces/IFactura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial3.Interfaces
+{
+    public interface IFactura
+    {
+        void GetType();
+        void GetNumber();
+        void GetDate();
+        void GetAmountTotal();
+        void SetType(string type);
+        void SetNumber(int number);
+        void SetDate(DateTime date);
+        void SetAmountTotal(int amountTotal);
+        void ShowPreviewInvoice();
+        void ReadInvoice(int id);
+    }
+}
